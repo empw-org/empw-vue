e92ac9389ee1ccd39d0b3f72e9cf1629c2494a9f
Alaa Othman <alaa.othman4707@gmail.com>
2020-07-21 21:05:31 +0200

    add GradientButton components

---
+++ src/components/GradientButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button><slot></slot></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="less">
+.gradient {
+  background: linear-gradient(to right, #00bdce 6%, #0071d2);
+}
+
+button {
+  .gradient();
+  border: 2px solid white;
+  color: white;
+  border-radius: 2em;
+  padding: 15px;
+  font-weight: 600;
+  font-size: 20px;
+  width: 100%;
+  margin-top: 10px;
+  transition: all 0.3s;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
