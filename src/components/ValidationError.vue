fdf62139e739bcb0437e4cd419da91f6bab7932e
Alaa Othman <alaa.othman4707@gmail.com>
2020-07-22 16:21:04 +0200

    add ValidationError component

---
+++ src/components/ValidationError.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span class="error-message"> <slot></slot> </span>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped lang="less">
+.error-message {
+  font-size: 10pt;
+  color: red;
+  margin-top: 2px;
+}
+</style>
