5d93b4ebd30a6d36a69978de48c1f2d53a3c85b1
Alaa Othman <alaa.othman4707@gmail.com>
2020-07-25 23:43:22 +0200

    fix sign in route in company sign up

--- src/views/company/SignUp.vue
+++ src/views/company/SignUp.vue
@@ -98,7 +98,7 @@
 
     <div class="have-account-text">
       Already have account?
-      <router-link to="/user/signin" class="sign-in-link"
+      <router-link to="/company/signin" class="sign-in-link"
         >Sign in now</router-link
       >
     </div>
